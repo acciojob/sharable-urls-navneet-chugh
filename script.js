766cfb2b37c1db5d57084b7007abc19dea19124b
navneet-chugh <navneetchugh15@gmail.com>
2023-08-23 21:09:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // your code here
+const urlForm = document.getElementById("urlForm");
+    const urlText = document.getElementById("url");
+    
+    urlForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+      
+      const name = encodeURIComponent(document.getElementById("name").value);
+      const year = encodeURIComponent(document.getElementById("year").value);
+      
+      const queryString = `?name=${name}&year=${year}`;
+      const updatedURL = `https://localhost:8080/${queryString}`;
+      
+      urlText.textContent = updatedURL;
+    });
